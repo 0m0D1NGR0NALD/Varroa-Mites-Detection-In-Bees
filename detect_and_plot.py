78c51bcfc286ff996c979f7b71217ab17a1b19b6
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-05-09 10:39:02 +0300

    Update detect_and_plot.py

--- detect_and_plot.py
+++ detect_and_plot.py
@@ -7,20 +7,20 @@ def detectx (frame, model):
     """
     >>> This function takes frame and model
     """
-  frame = [frame]
-  print(f"[INFO] Detecting. . . ")
-  results = model(frame)
-  # Display results
-  results.show()
-  # Frame predictions 
-  print(results.xyxyn[0]) # Tensor
-  print(results.pandas().xyxyn[0]) # Pandas
-  print(results.xyxyn[0][:, -1]) # Tensor
-  print(results.xyxyn[0][:, :-1]) # Tensor
-  # Extract class label and BBox coordinates
-  label, coordinates = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
-  # Return label and respective coordinates
-  return label, coordinates
+    frame = [frame]
+    print(f"[INFO] Detecting. . . ")
+    results = model(frame)
+    # Display results
+    results.show()
+    # Frame predictions 
+    print(results.xyxyn[0]) # Tensor
+    print(results.pandas().xyxyn[0]) # Pandas
+    print(results.xyxyn[0][:, -1]) # Tensor
+    print(results.xyxyn[0][:, :-1]) # Tensor
+    # Extract class label and BBox coordinates
+    label, coordinates = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
+    # Return label and respective coordinates
+    return label, coordinates
 
 # Plot the BBox and results
 def plot_boxes(results, frame,classes):
@@ -33,3 +33,5 @@ def plot_boxes(results, frame,classes):
     print(labels)
     n = len(labels)
     x_shape, y_shape = frame.shape[1], frame.shape[0]
+    
+    print(f"[INFO] Total {n} detections. . . ")
