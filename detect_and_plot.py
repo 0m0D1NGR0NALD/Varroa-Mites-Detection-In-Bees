58c0e413e00302a34a7cc2dfbb27fee69387e5c2
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-05-09 10:50:25 +0300

    Update detect_and_plot.py

--- detect_and_plot.py
+++ detect_and_plot.py
@@ -51,3 +51,5 @@ def plot_boxes(results, frame,classes):
             if text == 'varroa-mite':
                 # BBox
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+                # Labels
+                cv2.putText(frame, text_d, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255,255,255), 1)
